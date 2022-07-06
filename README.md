eb605d2206795731533fb292783536b3b720e225
Raphael Saldanha <raphael.saldanha@icict.fiocruz.br>
2022-07-06 04:50:02 +0000

    version 0.1.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# brpop
+R data package with yearly (2000 to 2021) Brazilian population estimates from DataSUS/Brazilian Health Ministry, aggregated by state, municipality, sex, and age groups.
---
+++ tests/testthat/test-pop_uf_consistency.R
@@ -0,0 +1,36 @@
+test_that("pop uf general is consistent", {
+  res <- uf_pop() %>%
+    dplyr::group_by(year, age_group) %>%
+    dplyr::summarise(freq = sum(pop, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(freq) %>%
+    dplyr::summarise(count = dplyr::n()) %>%
+    dplyr::filter(count > 1)
+
+  expect_equal(nrow(res), 0)
+})
+
+test_that("pop uf male is consistent", {
+  res <- uf_male_pop %>%
+    dplyr::group_by(year, age_group) %>%
+    dplyr::summarise(freq = sum(pop, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(freq) %>%
+    dplyr::summarise(count = dplyr::n()) %>%
+    dplyr::filter(count > 1)
+
+  expect_equal(nrow(res), 0)
+})
+
+test_that("pop uf female is consistent", {
+  res <- uf_female_pop %>%
+    dplyr::group_by(year, age_group) %>%
+    dplyr::summarise(freq = sum(pop, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(freq) %>%
+    dplyr::summarise(count = dplyr::n()) %>%
+    dplyr::filter(count > 1)
+
+  expect_equal(nrow(res), 0)
+})
+
